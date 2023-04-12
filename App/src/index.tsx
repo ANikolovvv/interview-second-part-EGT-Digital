bf3d93378193977828241130f2762c72c9cb5463
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-04-12 18:53:33 +0300

    Update index.tsx

--- App/src/index.tsx
+++ App/src/index.tsx
@@ -7,13 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Provider store={store} >
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
