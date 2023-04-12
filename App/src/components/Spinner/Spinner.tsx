98da7f1f101b9f252b207862ca5accff31958ecf
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-04-12 18:48:27 +0300

    Update Spinner.tsx

--- App/src/components/Spinner/Spinner.tsx
+++ App/src/components/Spinner/Spinner.tsx
@@ -1,8 +1,8 @@
 import { Spin } from "antd";
-
+import styles from './Spinner.module.css'
 const Spinner: React.FC = () => {
   return (
-    <div className="loading-container">
+    <div className={styles["loading-container"]}>
       <Spin size="large" />
       <h1>Loading data, please wait...</h1>
     </div>
